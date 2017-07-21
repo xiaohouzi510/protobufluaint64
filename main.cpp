bde9ad3ce9209381c802193c349b7914fa70d6db
yongdehuang <yongdehuang@TwGame.(none)>
2017-07-21 14:25:34 +0800

    参数个数错误

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 int main(int argc,char *argv[])
 {
-	if(argc < 1)
+	if(argc < 2)
 	{
 		printf("param must be teamID\n");
 		return 0;
--- test.lua
+++ test.lua
@@ -1,8 +1,13 @@
-local sStr = ...
+local sStr1 = ...
 package.path = package.path..";./proto_lua/?.lua;./protobuf_lua/?.lua"
 require "pb"
 require "test_pb"
 
-local stProto = test_pb.SSInt64Test()
-stProto:ParseFromString(sStr)
-print(tostring(stProto))
+local stProto1 = test_pb.SSInt64Test()
+stProto1:ParseFromString(sStr1)
+print(tostring(stProto1))
+
+local stProto2 = test_pb.SSInt64Test()
+local sStr2 = stProto1:SerializeToString()
+stProto2:ParseFromString(sStr1)
+print(tostring(stProto2))
